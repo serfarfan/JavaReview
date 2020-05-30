ba441f0806309cd715cf80d89ecfcfad43dac2d0
sergio <serfar260@hotmail.com>
2020-05-29 20:14:28 -0500

    Clase 4 Repaso de java (Primitivos, arrays, listas )  LinearLayout

--- app/src/main/java/com/cahs/javareview/MainActivity.java
+++ app/src/main/java/com/cahs/javareview/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private static String ETIQUETA = "Review";
@@ -14,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(ETIQUETA,"Entra al onCreate");
+        //manageStrings("Carlos", "Hernández");
+        //manageInts(78, 69);
+        int[] arrInts = new int[] {2, 4, 6};
+        double[] arrDoubles = new double[] {3.0, 5.0, 7.0};
+        //manageArrays(arrInts, arrDoubles);
+        //Lista de Enteros
+        List<Integer> integers = new ArrayList<>();
+        for (int index = 0; index < 5; index++) integers.add(index*2);
+        //Lista de Strings
+        List<String> stringList = new ArrayList<>();
+        for (int index = 0; index < 4; index++) stringList.add("Palabra " + index);
+        manageLists(integers, stringList);
     }
 
     @Override
@@ -51,4 +66,51 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.w(ETIQUETA, "Entra al onDestroy");
     }
+
+    //Unir cadenas
+    private void manageStrings(String first, String second){
+        String result = first + " " + second;
+        boolean isValid;
+        isValid = true;
+        if (isValid) Log.d(ETIQUETA, "Mi nombre es: " + result);
+        else Log.d(ETIQUETA, "No se halló un nombre " );
+    }
+
+    //Ints
+    private void manageInts(int first, int second){
+        //suma
+        int sum = first + second;
+        //Resta
+        int substraction = first - second;
+        //Multiplicación
+        int product = first * second;
+        //División
+        double division = first/second;
+
+        Log.d(ETIQUETA, "Suma es: " + sum);
+        Log.d(ETIQUETA, "Resta es: " + substraction);
+        Log.d(ETIQUETA, "Multiplicación es: " + product );
+        Log.d(ETIQUETA, "División es: " + division);
+    }
+
+    //Arrays
+    private void manageArrays(int [] arrayInt, double [] doubleArray){
+        //Iterar sobre cada array
+        for (int i = 0; i < doubleArray.length; i++)
+            /*Log.d(ETIQUETA, "Element at index " + i +
+                    " : "+ arrayInt[i]);*/
+        Log.d(ETIQUETA, "Element double at index " + i +
+                    " : "+ doubleArray[i]);
+
+    }
+
+    //Listas
+    private void manageLists(List<Integer> integers, List<String> stringList){
+        //Iterar sobre cada array
+        for (int i = 0; i < stringList.size(); i++)
+            Log.d(ETIQUETA, "Elemento cadena en el índice " + i +
+                    " : "+ stringList.get(i));
+    }
+
+
 }
